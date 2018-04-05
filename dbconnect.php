26b510da43e4290f2c1bc3361cf74bba29984324
Prashant Rizal <RizalPrashant@Prashants-MacBook-Pro.local>
2018-04-05 02:01:43 -0600

    Using dbconnect.php for database storage connection

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$username = 'root';
-$password = 'root';
-$database_name = 'UserInfo';
+$host = 'us-cdbr-iron-east-05.cleardb.net';
+$username = 'ba97471f656d10';
+$password = '37e90f86';
+$database_name = 'heroku_89d03e637463650';
 
 $connection = mysqli_connect($host, $username, $password, $database_name);
 if(mysqli_connect_errno()):
