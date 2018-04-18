4101f327eee100b9c97c0bd6684d9854ebfb5165
Prashant Rizal <Prashant@Prashants-MacBook-Pro.local>
2018-04-18 16:54:23 -0600

    safd

--- displayfeedback.php
+++ displayfeedback.php
@@ -7,10 +7,10 @@
     </head>
     <body>
 <?php
-    include('dbconnect.php'); 
+    require_once('dbconnect.php'); 
         echo "<br>";
         $my_query = "";
-    $my_query = "select * from Feedback";
+    $my_query = "select * from feedback";
     $result = mysqli_connect($connection, $my_query) or die('error');
         echo '<table class="greenTable">';
 echo "<thead>";
@@ -18,7 +18,7 @@ echo "<tr> <th>Name</th> <th>Email</th> <th>Feedback</th> </tr>";
 echo "</thead>";
 echo "<tbody>";
         while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-        echo "<tr> <td>cell1_1</td> <td>cell2_1</td> <td>cell3_1</td> </tr>";
+        echo "<tr> <td> $row['Name']</td> <td>$row['Email']</td> <td>$row['suggestion']</td> </tr>";
         }
 
 
