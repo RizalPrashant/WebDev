473cab72ac8f7955a1fa8e855bba22c554e46efb
Prashant Rizal <Prashant@Prashants-MacBook-Pro.local>
2018-04-18 13:41:16 -0600

    table

--- displayfeedback.php
+++ displayfeedback.php
@@ -10,43 +10,20 @@
     require_once('dbconnect.php');    
     $my_query = "SELECT * from User";
     $result = mysqli_connect($connection, $my_query);
+        echo "<table class="greenTable">";
+echo "<thead>";
+echo "<tr> <th>head1</th> <th>head2</th> <th>head3</th> </tr>";
+echo "</thead>";
+echo "<tbody>";
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        echo "<tr> <td>cell1_1</td> <td>cell2_1</td> <td>cell3_1</td> </tr>"
+        }
+
+
+echo "</tbody>";
+echo "</table>";
       ?>  
-   <table class="greenTable">
-<thead>
-<tr>
-<th>head1</th>
-<th>head2</th>
-<th>head3</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>cell1_1</td>
-<td>cell2_1</td>
-<td>cell3_1</td>
-</tr>
-<tr>
-<td>cell1_2</td>
-<td>cell2_2</td>
-<td>cell3_2</td>
-</tr>
-<tr>
-<td>cell1_3</td>
-<td>cell2_3</td>
-<td>cell3_3</td>
-</tr>
-<tr>
-<td>cell1_4</td>
-<td>cell2_4</td>
-<td>cell3_4</td>
-</tr>
-<tr>
-<td>cell1_5</td>
-<td>cell2_5</td>
-<td>cell3_5</td>
-</tr>
-</tbody>
-</table>
+   
 
     </body>
 </html>
