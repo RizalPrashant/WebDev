79947b2a473c536722e31ca42eab380549dea86e
Prashant Rizal <Prashant@Prashants-MacBook-Pro.local>
2018-04-23 07:21:22 -0600

    Sdf

--- trylogging.php
+++ trylogging.php
@@ -11,7 +11,7 @@ $my_query = "select * from User where Username = '$uname' and Password = '$passw
 
 $result= mysqli_query($connection, $my_query);
 if(mysqli_num_rows($result) > 0){
-    $_SESSION['uname'] = $uname;
+    $_SESSION["uname"] = $uname;
     header("Location: home.php"); 
     exit;
 }
